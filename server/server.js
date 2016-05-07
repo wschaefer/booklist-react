692edf94a1a83dcc603fa41206a0d9ed96219f59
Whitney Schaefer <whitney.tyson@gmail.com>
2016-05-07 18:12:57 +0900

    Setting up to push to cf

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ var path = require("path");
 
 var app = express();
 app.use(express.static(path.join(__dirname,"../app/build")));
-app.listen(7777,function(){
-    console.log("Started listening on port", 7777);
+app.listen(process.env.PORT || 7777,function(){
+    console.log("Started listening on port", process.env.PORT || 7777);
 })
